c9fec0dff555d3d05d4b1ef1eac52a890622ca49
tpeinoit <toni.peinoit@gmail.com>
2024-11-17 22:54:59 +0100

    v.1.1.0

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.5
 import PackageDescription
 
 let package = Package(
     name: "GoMarketMe",
     platforms: [
-        .iOS(.v13)  // This ensures all dependencies are built with iOS in mind
+        .iOS(.v15)  // This ensures all dependencies are built with iOS in mind
     ],
     products: [
         .library(
@@ -12,23 +12,11 @@ let package = Package(
             targets: ["GoMarketMe"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.4.0"),
-        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "5.0.0"),
-        .package(url: "https://github.com/mxcl/PromiseKit.git", from: "6.13.3"),
-        .package(url: "https://github.com/ashleymills/Reachability.swift.git", from: "5.0.0"),
-        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.3.0"),
-        .package(url: "https://github.com/devicekit/DeviceKit.git", .upToNextMajor(from: "4.0.0"))
     ],
     targets: [
         .target(
             name: "GoMarketMe",
             dependencies: [
-                "Alamofire",
-                "SwiftyJSON",
-                "PromiseKit",
-                .product(name: "Reachability", package: "Reachability.swift"),
-                "CryptoSwift",
-                .product(name: "DeviceKit", package: "DeviceKit")
             ],
             path: "Sources"
         ),
--- Sources/GoMarketMe/GoMarketMe.swift
+++ Sources/GoMarketMe/GoMarketMe.swift
@@ -2,91 +2,145 @@ import Foundation
 import UIKit
 import StoreKit
 
-public class GoMarketMe: NSObject, SKPaymentTransactionObserver {
+public class GoMarketMe: NSObject, ObservableObject, SKRequestDelegate {
     public static let shared = GoMarketMe()
+    private var paymentQueueObserver: NSObjectProtocol?
     private let sdkInitializedKey = "GOMARKETME_SDK_INITIALIZED"
-    private var affiliateCampaignCode: String = ""
-    private var deviceId: String = ""
+    private var affiliateCampaignCode: String?
+    private var deviceId: String?
     private var apiKey: String = ""
     private let sdkInitializationUrl = URL(string: "https://api.gomarketme.net/v1/sdk-initialization")!
     private let systemInfoUrl = URL(string: "https://api.gomarketme.net/v1/mobile/system-info")!
     private let eventUrl = URL(string: "https://api.gomarketme.net/v1/event")!
 
+    private var updateListenerTask: Task<Void, Error>? = nil
+    private var currTransaction: Transaction? = nil
+    
     private override init() {
         super.init()
-        // Additional initialization if necessary
     }
-
+    
     public func initialize(apiKey: String) {
         self.apiKey = apiKey
-        if !isSDKInitialized() {
-            postSDKInitialization(apiKey: apiKey)
-        }
-        self.getSystemInfo { systemInfo in
-            self.postSystemInfo(systemInfo: systemInfo, apiKey: apiKey) {
-                self.addListener(apiKey: apiKey)
+
+        Task {
+            await startListeningForUpdates()
+
+            do {
+                if !(await isSDKInitialized()) {
+                    try await postSDKInitialization(apiKey: apiKey)
+                }
+
+                let systemInfo = await getSystemInfo()
+                try await postSystemInfo(systemInfo: systemInfo, apiKey: apiKey)
+
+                for await result in Transaction.all {
+                    if affiliateCampaignCode != nil {
+                        print("RE Processing pending transaction")
+                        await processTransactionResult(result)
+                    }
+                }
+                
+            } catch {
+                print("Error during SDK initialization: \(error.localizedDescription)")
             }
         }
     }
 
-    private func addListener(apiKey: String) {
-        SKPaymentQueue.default().add(self)
+    public func startListeningForUpdates() async {
+        updateListenerTask = Task {
+            for await result in Transaction.currentEntitlements {
+                if affiliateCampaignCode != nil {
+                    await processTransactionResult(result)
+                }
+            }
+        }
+    }
+    
+    func fetchAndSendProduct(productId:String) async {
+        do {
+            let product = try await Product.products(for: [productId])
+            if !product.isEmpty {
+                sendProductDetails(product[0])
+            }
+
+        } catch {
+            print("Failed to fetch products: \(error)")
+        }
     }
 
-    private func postSDKInitialization(apiKey: String) {
+    private func processTransactionResult(_ result: VerificationResult<Transaction>) async {
+        switch result {
+        case .verified(let transaction):
+            await fetchAndSendProduct(productId: transaction.productID)
+            fetchPurchaseDetails(transaction: transaction)
+        case .unverified(let transaction, _):
+            await fetchAndSendProduct(productId: transaction.productID)
+            fetchPurchaseDetails(transaction: transaction)
+        }
+    }
+        
+    deinit {
+        updateListenerTask?.cancel()
+    }
+
+    private func postSDKInitialization(apiKey: String) async throws {
         var request = URLRequest(url: sdkInitializationUrl)
         request.httpMethod = "POST"
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
         request.addValue(apiKey, forHTTPHeaderField: "x-api-key")
-        let task = URLSession.shared.dataTask(with: request) { data, response, error in
-            if let httpRes = response as? HTTPURLResponse, httpRes.statusCode == 200 {
-                self.markSDKAsInitialized()
-            } else {
-                print("Error marking the SDK as initialized")
-            }
+        let (_, response) = try await URLSession.shared.data(for: request)
+        if let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 {
+            markSDKAsInitialized()
+        } else {
+            throw NSError(domain: "GoMarketMeSDK", code: (response as? HTTPURLResponse)?.statusCode ?? 0, userInfo: [NSLocalizedDescriptionKey: "Failed to mark SDK as initialized."])
         }
-        task.resume()
     }
 
-    private func getSystemInfo(completion: @escaping ([String: Any]) -> Void) {
+    private func getSystemInfo() async -> [String: Any] {
         var systemInfo = [String: Any]()
-        let device = UIDevice.current
-        systemInfo["systemName"] = device.systemName
-        systemInfo["systemVersion"] = device.systemVersion
-        systemInfo["model"] = device.model
-        systemInfo["localizedModel"] = device.localizedModel
-        systemInfo["identifierForVendor"] = device.identifierForVendor?.uuidString
-        systemInfo["isPhysicalDevice"] = !isSimulator
-
-        let screen = UIScreen.main
-        systemInfo["devicePixelRatio"] = screen.scale
-        systemInfo["width"] = screen.bounds.width
-        systemInfo["height"] = screen.bounds.height
-
-        systemInfo["time_zone_code"] = TimeZone.current.identifier
-        systemInfo["language_code"] = Locale.current.languageCode
-
-        completion(systemInfo)
+        var deviceInfo = [String: Any]()
+        let device = await UIDevice.current
+        deviceInfo["systemName"] = await device.systemName
+        deviceInfo["systemVersion"] = await device.systemVersion
+        deviceInfo["model"] = await device.model
+        deviceInfo["localizedModel"] = await device.localizedModel
+        deviceInfo["identifierForVendor"] = await device.identifierForVendor?.uuidString
+        deviceInfo["isPhysicalDevice"] = !isSimulator
+        systemInfo["device_info"] = deviceInfo
+        var windowInfo = [String: Any]()
+        let screen = await UIScreen.main
+        windowInfo["devicePixelRatio"] = await screen.scale
+        windowInfo["width"] = await screen.bounds.width * screen.scale
+        windowInfo["height"] = await screen.bounds.height
+        systemInfo["window_info"] = windowInfo
+        systemInfo["time_zone"] = TimeZone.current.identifier
+        systemInfo["language_code"] = Locale.current.languageCode ?? "en"
+        return systemInfo
     }
 
-    private func postSystemInfo(systemInfo: [String: Any], apiKey: String, completion: @escaping () -> Void) {
+    private func postSystemInfo(systemInfo: [String: Any], apiKey: String) async throws {
         var request = URLRequest(url: systemInfoUrl)
         request.httpMethod = "POST"
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
         request.addValue(apiKey, forHTTPHeaderField: "x-api-key")
-        request.httpBody = try? JSONSerialization.data(withJSONObject: ["device_info": systemInfo])
-        let task = URLSession.shared.dataTask(with: request) { data, response, error in
-            if let httpRes = response as? HTTPURLResponse, httpRes.statusCode == 200 {
-                if let data = data, let jsonResponse = try? JSONSerialization.jsonObject(with: data) as? [String: Any] {
-                    self.handlePostSystemInfoResponse(jsonResponse)
-                    print("SystemInfo sent successfully")
-                    completion()
+        request.httpBody = try JSONSerialization.data(withJSONObject: systemInfo)
+        let (data, response) = try await URLSession.shared.data(for: request)
+        if let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 {
+            if let jsonObject = try? JSONSerialization.jsonObject(with: data, options: []),
+               let jsonDict = jsonObject as? [String: Any] {
+                affiliateCampaignCode = jsonDict["affiliate_campaign_code"] as? String
+                deviceId = jsonDict["device_id"] as? String
+                if deviceId != nil {
+                    print(deviceId!)
                 }
+                print("SystemInfo sent successfully")
             } else {
-                print("Failed to send SystemInfo. Status code: \((response as? HTTPURLResponse)?.statusCode ?? 0)")
+                throw NSError(domain: "GoMarketMeSDK", code: 1, userInfo: [NSLocalizedDescriptionKey: "Failed to parse response JSON."])
             }
+        } else {
+            throw NSError(domain: "GoMarketMeSDK", code: (response as? HTTPURLResponse)?.statusCode ?? 0, userInfo: [NSLocalizedDescriptionKey: "Failed to send SystemInfo."])
         }
-        task.resume()
     }
 
     private var isSimulator: Bool {
@@ -97,96 +151,91 @@ public class GoMarketMe: NSObject, SKPaymentTransactionObserver {
         #endif
     }
 
+    private func isSDKInitialized() async -> Bool {
+        return UserDefaults.standard.bool(forKey: sdkInitializedKey)
+    }
+
     func markSDKAsInitialized() {
         UserDefaults.standard.set(true, forKey: sdkInitializedKey)
     }
 
-    func isSDKInitialized() -> Bool {
-        return UserDefaults.standard.bool(forKey: sdkInitializedKey)
+    private func sendProductDetails(_ product: Product) {
+        let productInfo = [
+            "productID": product.id,
+            "productTitle": product.displayName,
+            "productDescription": product.description,
+            "productPrice": product.displayPrice,
+            "productRawPrice": product.price,
+            "productCurrencyCode": product.priceFormatStyle.currencyCode,
+            "productCurrencySymbol": product.priceFormatStyle.currencyCode,
+            "hashCode": product.hashValue
+        ] as [String: Any]
+        
+        self.sendEventToServer(eventType: "product", body: productInfo)
     }
 
-    public func paymentQueue(_ queue: SKPaymentQueue, updatedTransactions transactions: [SKPaymentTransaction]) {
-        for transaction in transactions {
-            switch transaction.transactionState {
-            case .purchased, .restored:
-                if !affiliateCampaignCode.isEmpty {
-                    completeTransaction(transaction)
-                }
-            case .failed:
-                failedTransaction(transaction)
-            case .deferred, .purchasing:
-                break
-            @unknown default:
-                break
-            }
+    func refreshReceipt() {
+        let request = SKReceiptRefreshRequest()
+        request.delegate = self
+        request.start()
+    }
+    
+    // Called when the receipt refresh completes successfully
+    public func requestDidFinish(_ request: SKRequest) {
+        if let receiptURL = Bundle.main.appStoreReceiptURL,
+            let receiptData = try? Data(contentsOf: receiptURL) {
+            let base64EncodedReceipt = receiptData.base64EncodedString()
+            sendPurchaseDetails(self.currTransaction!, receipt: base64EncodedReceipt)
+        } else {
+            print("Receipt is still missing.")
         }
     }
-
-    private func handlePostSystemInfoResponse(_ response: [String: Any]) {
-        if let affiliateCode = response["affiliate_campaign_code"] as? String {
-            self.affiliateCampaignCode = affiliateCode
-        }
-        if let deviceID = response["device_id"] as? String {
-            self.deviceId = deviceID
-        }
+        
+    // Called if the receipt refresh fails
+    public func request(_ request: SKRequest, didFailWithError error: Error) {
+        print("Failed to refresh receipt: \(error.localizedDescription)")
+    }
+    
+    private func fetchPurchaseDetails(transaction: Transaction) {
+        self.currTransaction = transaction
+        refreshReceipt()
+    }
+    
+    private func sendPurchaseDetails(_ transaction: Transaction, receipt: String) {
+        let purchaseInfo: [String: Any] = [
+                "productID": transaction.productID,
+                "purchaseID": String(transaction.id),
+                "transactionDate": transaction.purchaseDate.timeIntervalSince1970,
+                "status": "",
+                "verificationData": [
+                    "localVerificationData": receipt,
+                    "source": "app_store"
+                ],
+                "pendingCompletePurchase": "",
+                "error": "",
+                "hashCode": String(transaction.hashValue)
+            ]
+        self.sendEventToServer(eventType: "purchase", body: purchaseInfo)
     }
 
-    private func completeTransaction(_ transaction: SKPaymentTransaction) {
-        let purchaseDetails = serializePurchaseDetails(transaction)
-        sendEventToServer(eventType: "purchase", body: purchaseDetails) {
-            SKPaymentQueue.default().finishTransaction(transaction)
-        }
-    }
-
-    private func failedTransaction(_ transaction: SKPaymentTransaction) {
-        if let error = transaction.error as NSError? {
-            print("Transaction Error: \(error.localizedDescription)")
-        }
-        SKPaymentQueue.default().finishTransaction(transaction)
-    }
-
-    private func sendEventToServer(eventType: String, body: [String: Any], completion: @escaping () -> Void) {
-        let url = URL(string: eventUrl.absoluteString + "?eventType=\(eventType)")!
-        var request = URLRequest(url: url)
+    private func sendEventToServer(eventType: String, body: [String: Any], completion: (() -> Void)? = nil) {
+        var request = URLRequest(url: eventUrl)
         request.httpMethod = "POST"
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
         request.addValue(self.apiKey, forHTTPHeaderField: "x-api-key")
-        request.addValue(deviceId, forHTTPHeaderField: "x-device-id")
-        request.addValue(affiliateCampaignCode, forHTTPHeaderField: "x-affiliate-campaign-code")
+        request.addValue(deviceId ?? "", forHTTPHeaderField: "x-device-id")
+        request.addValue(affiliateCampaignCode ?? "", forHTTPHeaderField: "x-affiliate-campaign-code")
+        request.addValue(eventType, forHTTPHeaderField: "x-event-type")
+        request.addValue("app_store", forHTTPHeaderField: "x-source-name")
         request.httpBody = try? JSONSerialization.data(withJSONObject: body)
-
         let task = URLSession.shared.dataTask(with: request) { data, response, error in
-            if let httpRes = response as? HTTPURLResponse, httpRes.statusCode == 200 {
-                print("\(eventType) sent successfully")
-                completion()
-            } else {
-                print("Failed to send \(eventType). Status code: \((response as? HTTPURLResponse)?.statusCode ?? 0)")
+            if let error = error {
+                print("Failed to send \(eventType): Network error: \(error.localizedDescription)")
+                return
             }
+            let response = response as? HTTPURLResponse
+            completion?()
         }
         task.resume()
-    }
-
-    private func serializePurchaseDetails(_ transaction: SKPaymentTransaction) -> [String: Any] {
-        var details = [String: Any]()
-        details["productID"] = transaction.payment.productIdentifier
-        details["transactionDate"] = transaction.transactionDate?.timeIntervalSince1970 ?? 0
-        details["status"] = transaction.transactionState.rawValue  // Enum to raw value
-        details["transactionID"] = transaction.transactionIdentifier ?? ""
-        
-        // Include additional fields as necessary
-        if transaction.transactionState == .restored {
-            details["originalTransactionID"] = transaction.original?.transactionIdentifier ?? ""
-        }
-
-        // Include error details if transaction failed
-        if let error = transaction.error as NSError? {
-            details["error"] = [
-                "code": error.code,
-                "domain": error.domain,
-                "userInfo": error.userInfo
-            ]
-        }
-
-        return details
     }
 }
